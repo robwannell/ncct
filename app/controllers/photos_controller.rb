61fb25736f6963fc877a4d15a25b158bc9afa478
robwannell <rob@meltemiweb.com>
2015-04-08 13:43:45 -0700

    pho

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  before_filter :require_login, except: [:show, :index ]
 
   # GET /photos
   # GET /photos.json

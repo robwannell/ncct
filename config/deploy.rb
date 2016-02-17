a7e79497ca92429bbfef66223668193fe24b9a93
robwannell <rob@meltemiweb.com>
2016-02-17 13:20:05 -0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@ set :application, 'ncct'
 set :repo_url, 'git@github.com:robwannell/ncct.git'
 
 set :deploy_to, '/home/deploy/ncct'
+set :rbenv_path, '/home/deploy/.rbenv/'
 
 set :linked_files, %w{config/database.yml config/secrets.yml config/initializers/setup_mail.rb}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}

f501162176e6931c61698db484f3772942558683
robwannell <rob@meltemiweb.com>
2016-02-23 11:58:03 -0800

    raffle home

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,9 @@ class WelcomeController < ApplicationController
   def index
     @news = Announcement.first
     @grads = Graduate.first
+    @raff = Raffle.first
+    if @raff.display == true
+      @raffle = @raff
+    end
   end
 end
--- app/models/raffle.rb
+++ app/models/raffle.rb
@@ -1,6 +1,6 @@
 class Raffle < ActiveRecord::Base
   
-  has_attached_file :avatar, :styles => { :medium => "750x600>", :thumb => "150x150>" }, :default_url => "missingproduct.png"
+  has_attached_file :avatar, :styles => { :medium => "750x600>", :thumb => "300x300>" }, :default_url => "missingproduct.png"
     # Validate content type
     validates_attachment_content_type :avatar, :content_type => /\Aimage/
     # Validate filename
